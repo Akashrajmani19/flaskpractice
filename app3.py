e16c8a5aced48859c115d2185d8c778a2f1f02da
Akash vaishnav <aakashrankawat19@gmail.com>
2023-07-29 10:30:13 +0530

    app3.py

---
+++ app3.py
@@ -0,0 +1,28 @@
+from flask import Flask,request,render_template
+
+
+app=Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return "<p>Welcome To the Flask.</p>"
+
+@app.route("/cal" methods =["GET"])
+def meth_operation():
+    operation = request.json["operation"]
+    number1 =request.json["number1"]
+    number2 =request.json["number2"]
+
+    if operation=="add":
+        result= number1+number2
+    elif operation=="multiply":
+        result = number1*number2
+    elif operation=="division":
+        result = number1/number2
+    else:
+        result = number1-number2
+    return result
+
+
+if __name__=="__main__":
+    app.run()
